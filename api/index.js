cf8da1d90ce6f26002248890402b706abdeec1a4
Sarvesh15a <yadsarvesh15@gmail.com>
2024-02-03 23:54:20 +0530

    Connect MangoDb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
-const app= express()
+
+dotenv.config();
+
+const app= express();
+
+//Database Connection
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Mangodb Connect")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
 
 app.listen(3000,()=>{
     console.log("Server is runing on port : 3000")
