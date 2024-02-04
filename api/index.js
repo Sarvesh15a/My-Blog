8fa344518c320659ffea44ab3153b78ee81dffe4
Sarvesh15a <yadsarvesh15@gmail.com>
2024-02-04 11:30:53 +0530

    API testing and Create Conteoller

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoute from '../api/routes/user.route.js'
 
 
 dotenv.config();
@@ -20,3 +21,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 app.listen(3000,()=>{
     console.log("Server is runing on port : 3000")
 })
+
+
+
+app.use("/api/user",userRoute)
