a96573dfbf65a1d73029be67b9daadd12f902675
Sarvesh15a <yadsarvesh15@gmail.com>
2024-02-14 22:14:21 +0530

    delete user functionlaty

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,15 +1,15 @@
-import { Button, TextInput,Alert } from 'flowbite-react'
+import { Button, TextInput,Alert, Modal } from 'flowbite-react'
 import React, { useState, useRef, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
 import { app } from './../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { updateStart,updateSuccess,updateFailure } from '../redux/user/userSlice';
-
+import { updateStart,updateSuccess,updateFailure,deleteUserStart,deleteUserSuccess, deleteUserFailure } from '../redux/user/userSlice';
+import { HiOutlineExclamationCircle } from 'react-icons/hi'
 
 const DashProfile = () => {
-    const {currentUser}=useSelector(state=> state.user)
+    const {currentUser,error}=useSelector(state=> state.user)
     const [imageFile , setImageFile]=useState(null)
     const [imageFileUrl,setImageFileUrl]=useState(null)
     const [imageFileUploadProgress,setImageFileUploadProgress]=useState(null)
@@ -18,6 +18,7 @@ const DashProfile = () => {
     const [imageFileUploading,setImageFileUploading]=useState(false)
     const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
     const [updateUserError, setUpdateUserError] = useState(null);
+    const [showModel,setShowModel]=useState(false)
     const filePickerRef=useRef()
     const dispatch=useDispatch();
     const handleImageChange=(e)=>{
@@ -118,6 +119,27 @@ const DashProfile = () => {
         setUpdateUserError(error.message);
       }
     };
+
+    const handleDeleteUser=async()=>{
+        setShowModel(false)
+
+        try {
+          dispatch(deleteUserStart());
+          const res=await fetch(`/api/user/delete/${currentUser._id}`,{
+            method:'DELETE',
+          })
+          const data=await res.json();
+          if(!res.ok){
+            return dispatch(deleteUserFailure(data.message))
+          }
+          else{
+            return dispatch(deleteUserSuccess(data))
+          }
+
+        } catch (error) {
+          dispatch(deleteUserFailure(error.message))
+        }
+    }
   return (
 
     <div className='max-w-lg mx-auto p-3 w-full'>
@@ -191,7 +213,7 @@ const DashProfile = () => {
            </Button>
         </form>
         <div className="text-red-500 flex justify-between mt-5">
-            <span className='cursor-pointer'>Delete Account</span>
+            <span onClick={()=>setShowModel(true)} className='cursor-pointer'>Delete Account</span>
             <span className='cursor-pointer'>Sign Out</span>
         </div>
 
@@ -205,6 +227,41 @@ const DashProfile = () => {
             {updateUserError}
           </Alert>
         )}
+        {error && (
+          <Alert color='failure' className='mt-5'>
+            {error}
+          </Alert>
+        )}
+         
+         
+         {/* show model  */}
+          <Modal
+            show={showModel}
+            onClose={()=>setShowModel(false)}
+            popup
+            size='md'>
+            
+            <Modal.Header>
+              <Modal.Body>
+                <div className="text-center">
+                  <HiOutlineExclamationCircle 
+                  className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto'/>
+                  <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+                    Are you sure you want to delete your account?
+                  </h3>
+                  <div className="flex justify-center gap-4">
+                    <Button color='failure' onClick={handleDeleteUser}>
+                      Yes, I'm sure
+                    </Button>
+                    <Button color='gray' onClick={()=>setShowModel(false)}>
+                      NO,cancel
+                    </Button>
+                  </div>
+                </div>
+              </Modal.Body>
+            </Modal.Header>
+          </Modal>
+
         </div>
   )
 }
