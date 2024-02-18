49911b56255eab43fbcd600a9c1d90f58bd243ad
Sarvesh15a <yadsarvesh15@gmail.com>
2024-02-18 21:19:10 +0530

    complete create post funcinilatis

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,15 +1,10 @@
 import { Button,Alert, FileInput, Select, TextInput } from 'flowbite-react';
-
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import {
-  getDownloadURL,
-  getStorage,
-  ref,
-  uploadBytesResumable,
-} from 'firebase/storage';
+import { getDownloadURL,getStorage,ref, uploadBytesResumable,} from 'firebase/storage';
 import { app } from '../firebase';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
@@ -19,6 +14,10 @@ const CreatePost = () => {
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
   const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
+
+  const navigate = useNavigate();
+
   const handleUpdloadImage = async () => {
     try {
       if (!file) {
@@ -30,11 +29,8 @@ const CreatePost = () => {
       const fileName = new Date().getTime() + '-' + file.name;
       const storageRef = ref(storage, fileName);
       const uploadTask = uploadBytesResumable(storageRef, file);
-      uploadTask.on(
-        'state_changed',
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      uploadTask.on('state_changed',(snapshot) => {
+          const progress =(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
           setImageUploadProgress(progress.toFixed(0));
         },
         (error) => {
@@ -55,13 +51,46 @@ const CreatePost = () => {
       console.log(error);
     }
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/post/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      }
+
+      else{
+        setPublishError(null);
+        navigate(`/post/${data.slug}`);
+      }
+    } catch (error) {
+      setPublishError('Something went wrong');
+    }
+  };
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
         <h1 className='text-center text-3xl my-7 font-semibold'> Create a Post</h1>
-        <form className='flex flex-col gap-4'>
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div className="flex flex-col gap-4 sm:flex-row justify-between">
-                <TextInput type='text' placeholder='Title' required id='title' className='flex-1'/>
-                 <Select>
+                <TextInput type='text' 
+                placeholder='Title'
+                 required id='title'
+                  className='flex-1'
+                  onChange={(e) =>
+                    setFormData({ ...formData, title: e.target.value })
+                  }/>
+                 <Select    onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }>
                     <option value='uncategorized' > Select a category</option>
                     <option value='javascript' > JavaScript</option>
                     <option value='reactjs' >React.js</option>
@@ -99,8 +128,19 @@ const CreatePost = () => {
                 />
             )
            }
-           <ReactQuill theme='snow' placeholder='Write something...' className='h-72 mb-12' required/>
+           <ReactQuill theme='snow'
+            placeholder='Write something...' 
+            className='h-72 mb-12'
+             required
+             onChange={(value) => {
+              setFormData({...formData, content: value });
+            }}/>
            <Button type='subimt' gradientDuoTone='purpleToPink'>Publish</Button>
+           {publishError && (
+          <Alert className='mt-5' color='failure'>
+            {publishError}
+          </Alert>
+        )}
         </form>
     </div>
   )
