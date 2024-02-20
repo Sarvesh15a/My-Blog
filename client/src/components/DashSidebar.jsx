31367553bcb4862518e369f96b5d74f57e91f7ca
Sarvesh15a <yadsarvesh15@gmail.com>
2024-02-20 18:14:44 +0530

    show uuser in admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import React,{useEffect, useState} from 'react'
 import { Sidebar } from 'flowbite-react';
-import { HiUser, HiArrowSmRight, HiDocumentText} from 'react-icons/hi'
+import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup} from 'react-icons/hi'
 import { Link, useLocation } from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
@@ -60,6 +60,18 @@ const DashSidebar = () => {
                         icon={HiDocumentText}
                         as='div'>
                           Posts
+                        </Sidebar.Item>
+                    </Link>
+                  )
+                }
+                {   
+                   currentUser.isAdmin && (
+                    <Link to='/dashboard?tab=users'>
+                        <Sidebar.Item 
+                        active={tab==='users'}
+                        icon={HiOutlineUserGroup}
+                        as='div'>
+                          Users
                         </Sidebar.Item>
                     </Link>
                   )
