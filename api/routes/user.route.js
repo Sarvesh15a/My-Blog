a6b86c971fbfae769e1e529cd69ed3d304c51e04
Sarvesh15a <yadsarvesh15@gmail.com>
2024-02-20 17:02:37 +0530

    create show user route api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express  from "express";
-import { signout, test, updateUser,deleteUser } from "../controller/user.controller.js";
+import { signout, test, updateUser,deleteUser, getUsers } from "../controller/user.controller.js";
 import {verifyToken} from '../utills/verifyUser.js'
 
 
@@ -9,5 +9,6 @@ router.get("/test",test);
 router.put('/update/:userId',verifyToken,updateUser)
 router.delete('/delete/:userId',verifyToken,deleteUser)
 router.post('/signout',signout)
+router.get('/getusers', verifyToken, getUsers);
 
 export default router;
