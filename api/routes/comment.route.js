0ece2136dcd8ca8162c93ee615d6e9b5dd43e326
Sarvesh15a <yadsarvesh15@gmail.com>
2024-02-22 15:01:20 +0530

    add comment functionality

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { verifyToken } from '../utills/verifyUser.js';
-import { createComment, getPostComments } from '../controller/comment.controller.js';
+import { createComment, editComment, getPostComments, likeComment } from '../controller/comment.controller.js';
 
 const router = express.Router();
 
 
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
+router.put('/editComment/:commentId', verifyToken, editComment);
 
 export default router;
